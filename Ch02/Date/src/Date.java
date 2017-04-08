3278179cf0efb410c03c6f89bb170f3ed3670f42
brianeflin <brian@Brians-MacBook-Pro.local>
2017-04-08 08:31:22 -0500

    Ch02 commit

---
+++ Ch02/Date/src/Date.java
@@ -0,0 +1,17 @@
+
+public class Date {
+
+	public static void main(String[] args) {
+		
+		String day = "Saturday";
+		int date = 8;
+		String month = "April";
+		int year = 2017;
+		
+		System.out.println("American format:");
+		System.out.println(day + ", " + month + " " + date + ", " + year);
+		System.out.println("European format:");
+		System.out.println(day + " " + date + " " + month + " " + year);
+	}
+
+}
