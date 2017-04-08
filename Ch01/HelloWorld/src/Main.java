b1f41aa1df123e81302419d78e3f63493036b36b
brianeflin <brian@Brians-MacBook-Pro.local>
2017-04-08 05:23:14 -0500

    Ch01 commit

---
+++ Ch01/HelloWorld/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// A superfluous comment.
+		System.out.println("Hello, Worlds!");
+		System.out.println("Welcome to the jungle.");
+	}
+
+}
